b4a321e098df4251f6419488dc558000f907b42a
SGhos176 <sghos176@its.jnj.com>
2024-06-18 15:11:56 +0530

    --added last-section

---
+++ frontend/src/Components/LastHolidaysSection/LastHolidayCard.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function LastHolidayCard() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default LastHolidayCard
---
+++ frontend/src/Components/LastHolidaysSection/LastHolidaysSection.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './LastHolidaysSection.css';
+
+export default function LastHolidaysSection() {
+  return (
+    <div>
+      LastH
+    </div>
+  )
+}

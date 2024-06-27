826672d9ea65a4121a34d273867b4993ca6dade4
subhamdraco <subham.smart97@gmail.com>
2024-06-28 00:09:38 +0530

    --merge

--- frontend/src/Components/Register/Register.jsx
+++ frontend/src/Components/Register/Register.jsx
@@ -5,15 +5,10 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import { MdTravelExplore } from "react-icons/md";
 import { Button , Form } from 'react-bootstrap';
-<<<<<<< HEAD
-
-export default function Register() {
-=======
 import axios from "axios";
 
 export default function Register() {
 
->>>>>>> 2ba50fccc99812fb3cd44a89cf4998fbae8cfcd8
     const [userplaceholder, setuserplaceholder] = useState('Username')
     const [validated, setValidated] = useState(false);
     const [formData , setformData] = useState({
@@ -30,10 +25,7 @@ export default function Register() {
         ag_pincode: '',
         username:'',
         password: '',
-<<<<<<< HEAD
-=======
         confirm_password: '',
->>>>>>> 2ba50fccc99812fb3cd44a89cf4998fbae8cfcd8
     })
 
     const onInputChange = (e) => {
@@ -44,35 +36,19 @@ export default function Register() {
             [name] : value
         };
     });
-<<<<<<< HEAD
-        setuserplaceholder(e.target.value)
-    };
-
-    const formsubmit = (e) =>{
-=======
         if (name==='mobile'){setuserplaceholder(e.target.value)};
     };
 
     const formsubmit = async  (e) =>{
->>>>>>> 2ba50fccc99812fb3cd44a89cf4998fbae8cfcd8
         e.preventDefault();
         const form = e.currentTarget;
         if (form.checkValidity() === false) {
           e.stopPropagation();
         }
-<<<<<<< HEAD
-    
-        setValidated(true);
-=======
->>>>>>> 2ba50fccc99812fb3cd44a89cf4998fbae8cfcd8
         const finaldata = {
             ...formData,
             username: formData.mobile
         }
-<<<<<<< HEAD
-
-        console.log(finaldata); //finaldata to be stored in db
-=======
         if (finaldata.password !== finaldata.confirm_password){e.stopPropagation(); alert('Passwords Do Not Match! '); return}
         setValidated(true);
         try{
@@ -85,7 +61,6 @@ export default function Register() {
         catch(e){
             console.log(e)
         }
->>>>>>> 2ba50fccc99812fb3cd44a89cf4998fbae8cfcd8
     }
 
   return (
@@ -120,11 +95,7 @@ export default function Register() {
                 </div>
                 <div className="col-lg-6">
                     <Form.Group className="mb-3 mt-3 w-75 h-100 shadow" controlId="last_name">
-<<<<<<< HEAD
-                        <Form.Control className='place h-100' type="text" placeholder="Last Name *  " required/>
-=======
                         <Form.Control className='place h-100' type="text" name="last_name" onChange={onInputChange} placeholder="Last Name *  " required/>
->>>>>>> 2ba50fccc99812fb3cd44a89cf4998fbae8cfcd8
                         <Form.Control.Feedback type="invalid">
                              Please provide a valid Last Name.
                         </Form.Control.Feedback> 
@@ -142,11 +113,7 @@ export default function Register() {
                 </div>
                 <div className="col-lg-4">
                     <Form.Group className="mb-3 w-75 h-100 shadow" controlId="email">
-<<<<<<< HEAD
-                        <Form.Control className='place h-100' type='email' placeholder="Email *" required/>
-=======
                         <Form.Control className='place h-100' name='email' onChange={onInputChange} type='email' placeholder="Email *" required/>
->>>>>>> 2ba50fccc99812fb3cd44a89cf4998fbae8cfcd8
                         <Form.Control.Feedback type="invalid">
                              Please provide a valid Email.
                         </Form.Control.Feedback> 
@@ -154,11 +121,7 @@ export default function Register() {
                 </div>
                 <div className="col-lg-4">
                     <Form.Group className="mb-3 w-75 h-100 shadow" controlId="whats_app">
-<<<<<<< HEAD
-                        <Form.Control className='place h-100' type='tel'  name='tel' pattern='[1-9]{1}[0-9]{9}' placeholder="WhatsApp *" required/>
-=======
                         <Form.Control className='place h-100' onChange={onInputChange} type='tel' name='whatsapp' pattern='[1-9]{1}[0-9]{9}' placeholder="WhatsApp *" required/>
->>>>>>> 2ba50fccc99812fb3cd44a89cf4998fbae8cfcd8
                         <Form.Control.Feedback type="invalid">
                              Please provide a valid Mobile Number.
                         </Form.Control.Feedback> 
@@ -173,58 +136,34 @@ export default function Register() {
             <div className="row head-row">
                 <div className="col-lg-6">
                     <Form.Group className="mb-3 mt-3 w-75 h-100 shadow" controlId="agency_name">
-<<<<<<< HEAD
-                        <Form.Control className='place h-100' type="text" placeholder="Agency Name" />
-=======
                         <Form.Control className='place h-100' name="ag_name" onChange={onInputChange}  type="text" placeholder="Agency Name" />
->>>>>>> 2ba50fccc99812fb3cd44a89cf4998fbae8cfcd8
                     </Form.Group>
                 </div>
                 <div className="col-lg-6">
                     <Form.Group className="mb-3 mt-3 w-75 h-100 shadow" controlId="agency_address">
-<<<<<<< HEAD
-                        <Form.Control className='place h-100' type="text" placeholder="Agency Address" />
-=======
                         <Form.Control className='place h-100' name="ag_add" onChange={onInputChange}  type="text" placeholder="Agency Address" />
->>>>>>> 2ba50fccc99812fb3cd44a89cf4998fbae8cfcd8
                     </Form.Group>
                 </div>
             </div>
             <div className="row mt-5 mb-3 sub-row">
                 <div className="col-lg-3">
                     <Form.Group className="mb-3 w-75 h-100 shadow" controlId="country">
-<<<<<<< HEAD
-                        <Form.Control className='place h-100' type="text" placeholder="Country"/>
-=======
                         <Form.Control className='place h-100' name="ag_country" onChange={onInputChange} type="text" placeholder="Country"/>
->>>>>>> 2ba50fccc99812fb3cd44a89cf4998fbae8cfcd8
                     </Form.Group>
                 </div>
                 <div className="col-lg-3">
                     <Form.Group className="mb-3 w-75 h-100 shadow" controlId="state">
-<<<<<<< HEAD
-                        <Form.Control className='place h-100' type="text" placeholder="State"/>
-=======
                         <Form.Control className='place h-100' name="ag_state" onChange={onInputChange} type="text" placeholder="State"/>
->>>>>>> 2ba50fccc99812fb3cd44a89cf4998fbae8cfcd8
                     </Form.Group>
                 </div>
                 <div className="col-lg-3">
                     <Form.Group className="mb-3 w-75 h-100 shadow" controlId="city">
-<<<<<<< HEAD
-                        <Form.Control className='place h-100' type="text" placeholder="City"/>
-=======
                         <Form.Control className='place h-100' name="ag_city" onChange={onInputChange} type="text" placeholder="City"/>
->>>>>>> 2ba50fccc99812fb3cd44a89cf4998fbae8cfcd8
                     </Form.Group>
                 </div>
                 <div className="col-lg-3">
                     <Form.Group className="mb-3 w-75 h-100 shadow" controlId="pin">
-<<<<<<< HEAD
-                        <Form.Control className='place h-100' type="number" placeholder="Pincode"/>
-=======
                         <Form.Control className='place h-100' name="ag_pincode" onChange={onInputChange} type="number" placeholder="Pincode"/>
->>>>>>> 2ba50fccc99812fb3cd44a89cf4998fbae8cfcd8
                     </Form.Group>
                 </div>
             </div>
@@ -239,11 +178,7 @@ export default function Register() {
                 </div>
                 <div className="col-lg-3 mb-3">
                     <Form.Group className="mb-3 w-75 h-100 shadow mb-4" controlId="password">
-<<<<<<< HEAD
-                        <Form.Control className='place h-100' type="password" placeholder="Password *"  autoComplete="off" required/>
-=======
                         <Form.Control className='place h-100' name="password" onChange={onInputChange} type="password" placeholder="Password *"  autoComplete="off" required/>
->>>>>>> 2ba50fccc99812fb3cd44a89cf4998fbae8cfcd8
                         <Form.Control.Feedback type="invalid">
                              Please provide a valid Password. Minimum 8 alphanumeric. 
                         </Form.Control.Feedback>
@@ -251,11 +186,7 @@ export default function Register() {
                 </div>
                 <div className="col-lg-3 mb-3">
                     <Form.Group className="mb-3 w-75 h-100 shadow mb-4" controlId="current_password">
-<<<<<<< HEAD
-                        <Form.Control className='place h-100' type="password" placeholder="Current Password *" autoComplete="off" required/>
-=======
                         <Form.Control className='place h-100' name="confirm_password" onChange={onInputChange} type="password" placeholder="Current Password *" autoComplete="off" required/>
->>>>>>> 2ba50fccc99812fb3cd44a89cf4998fbae8cfcd8
                         <Form.Control.Feedback type="invalid">
                             Error Password Do Not Match.
                         </Form.Control.Feedback>
